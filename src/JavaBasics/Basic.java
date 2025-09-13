25668d2ea76da7f37862c040b18b4de057acdb3a
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-09-14 01:13:25 +0530

    hello

---
+++ src/JavaBasics/Basic.java
@@ -0,0 +1,7 @@
+package JavaBasics;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println(511213 );
+    }
+}
---
+++ src/JavaBasics/Sum.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is : " + sum);
+    }
+}
